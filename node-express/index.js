9542f0a11119b469172c9d6a8deb30484e2f798d
Samhita Vetcha <52694492+vsamhita2028@users.noreply.github.com>
2020-10-31 14:32:45 +0530

    Serving html file using express

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
+const morgan = require("morgan");
+
 const hostname ='localhost';
 const port = 3000;
 
 const app= express();
+app.use(morgan('dev'));
 
+app.use(express.static(__dirname+"/public"))
 app.use((req,res,next)=>{
-    console.log(req.headers);
+   
     res.statuscode =200;
     res.setHeader("Content-Type","text/html");
 

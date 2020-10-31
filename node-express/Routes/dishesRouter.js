9bbc05648f6b02c1e53ee933733bfa4024885f95
Samhita Vetcha <52694492+vsamhita2028@users.noreply.github.com>
2020-10-31 17:45:31 +0530

    Express-Router

---
+++ node-express/Routes/dishesRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+dishRouter.route('/')
+.all( (req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+.get((req,res,next)=>{
+      res.end("We will get all the dishes");
+  })
+  .post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+  .put((req,res,next)=>{
+      req.statusCode= 403;
+      res.end("Put is not supported on /dishes");
+  })
+  .delete((req,res,next)=>{
+      res.end("We will delete all the dishes");
+  });
+
+  module.exports = dishRouter;

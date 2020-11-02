f86177f5cbdb621f6635796eb21498af0bfc3ff7
Samhita Vetcha <52694492+vsamhita2028@users.noreply.github.com>
2020-11-02 19:23:21 +0530

    Mongoose Part-2&3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -5,14 +5,28 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db)=>{
-    var newdish = dishes({
+     dishes.create({
         name : "Meow",
         description : "test"
-    });
-    newdish.save()
+    })
     .then((dish)=>{
         console.log(dish);
-        return dishes.find({})
+        return dishes.findByIdAndUpdate(dish._id,{
+            $set : {description : "Updated description"}
+        },
+        {
+            new : true
+        }).exec();
+    })
+    .then((dish)=>{
+        console.log(dish);
+
+        dish.comment.push({
+            rating : 5,
+            comment : "Dommu chua",
+            author : "boo"
+        });
+        return dish.save();
     })
     .then((dish)=>{
         console.log(dish);
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating :{
+        type: Number,
+        min :1,
+        max : 5,
+        required : true
+    },
+    comment :{
+        type : String,
+        required: true
+    },
+    author :{
+        type : String,
+        required : true
+    }   
+}, {
+    timestamps :true
+});
 const dishesSchema = new Schema({
     name :{
         type : String,
@@ -10,7 +28,8 @@ const dishesSchema = new Schema({
     description :{
         type : String,
         require : true,
-    }
+    },
+    comment : [commentSchema]
 },{
     timestamps : true
 }
